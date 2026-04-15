20ebc963ec8c4e88770152b7a2b051107d44e30e
Florian Obermayer <florian.obermayer@gmail.com>
2026-04-15 01:00:14 +0000

    sc++

--- app/csrf.py
+++ app/csrf.py
@@ -25,25 +25,25 @@ class FormFieldCSRFMiddleware(CSRFMiddleware):
         request = Request(scope, receive)
         csrf_cookie = request.cookies.get(self.cookie_name)
 
-        # Read the body eagerly so we can validate a form-field CSRF token.
-        # The ASGI receive callable can only be consumed once, so after reading
-        # we build a replay callable that returns the cached bytes to the downstream app.
-        body: bytes = await request.body()
-
-        _replayed = False
-
-        async def replay_receive() -> Message:
-            nonlocal _replayed
-            if not _replayed:
-                _replayed = True
-                return {"type": "http.request", "body": body, "more_body": False}
-            return {"type": "http.request", "body": b"", "more_body": False}
-
         if self._url_is_required(request.url) or (
             request.method not in self.safe_methods
             and not self._url_is_exempt(request.url)
             and self._has_sensitive_cookies(request.cookies)
         ):
+            # Read the body eagerly so we can validate a form-field CSRF token.
+            # The ASGI receive callable can only be consumed once, so after reading
+            # we build a replay callable that returns the cached bytes to the downstream app.
+            body: bytes = await request.body()
+
+            _replayed = False
+
+            async def replay_receive() -> Message:
+                nonlocal _replayed
+                if not _replayed:
+                    _replayed = True
+                    return {"type": "http.request", "body": body, "more_body": False}
+                return {"type": "http.request", "body": b"", "more_body": False}
+
             submitted_csrf_token = await self._get_submitted_csrf_token(request)
             if (
                 not csrf_cookie
@@ -54,8 +54,10 @@ class FormFieldCSRFMiddleware(CSRFMiddleware):
                 await response(scope, replay_receive, send)
                 return
 
-        send = functools.partial(self.send, send=send, scope=scope)
-        await self.app(scope, replay_receive, send)
+            send = functools.partial(self.send, send=send, scope=scope)
+            await self.app(scope, replay_receive, send)
+        else:
+            await self.app(scope, receive, send)
 
     async def _get_submitted_csrf_token(self, request: Request) -> str | None:
         header_token = await super()._get_submitted_csrf_token(request)
